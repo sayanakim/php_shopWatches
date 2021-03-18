485308d881b6cd1b928e812f9adab5d43eb0cba3
Sayana <sayana_0308@mail.ru>
2021-03-18 13:28:06 +0900

    second commit

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -4,7 +4,10 @@
 namespace app\controllers;
 
 
+use app\models\Breadcrumbs;
 use app\models\Category;
+use ishop\App;
+use ishop\libs\Pagination;
 
 class CategoryController extends AppController
 {
@@ -19,14 +22,24 @@ class CategoryController extends AppController
         }
 
         // хлебные крошки
-        $breadcrumbs = '';
+        $breadcrumbs = Breadcrumbs::getBreadcrumbs($category->id);
+
         $cat_model = new Category();
         $ids = $cat_model->getIds($category->id);
         $ids = !$ids ? $category->id : $ids . $category->id;
 
-        $products = \R::find('product', "category_id IN ($ids)");
+        // постраничная пагинация
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = App::$app->getProperty('pagination');
+        $total = \R::count('product', "category_id IN ($ids)");
+        $pagination = new Pagination($page, $perpage, $total);
+//        echo $pagination;
+        $start = $pagination->getStart();
+
+
+        $products = \R::find('product', "category_id IN ($ids) LIMIT $start, $perpage");
         $this->setMeta($category->title, $category->description, $category->keywords);
-        $this->set(compact('products', 'breadcrumbs'));
+        $this->set(compact('products', 'breadcrumbs', 'pagination', 'total'));
     }
 
 }
